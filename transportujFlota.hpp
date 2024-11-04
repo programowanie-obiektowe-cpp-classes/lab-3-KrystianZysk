e96f62d817db41d6841722d86d458438ba5e7609
KrystianZysk <zkrystian.z08@gmail.com>
2024-11-04 20:04:53 +0100

    Zadanie

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,20 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    if (towar==0) return 0;
+
+    unsigned int ilosc_Zaglowcow = 0;
+
+    Stocznia stocznia{};
+    while (stocznia.getTotalCap() < towar){
+        Statek* s1 = stocznia();
+
+        if(dynamic_cast<Zaglowiec*>(s1)){
+            ilosc_Zaglowcow = ilosc_Zaglowcow +1;
+        }
+
+        unsigned int transportowanyTowar = s1->transportuj();
+        delete s1;
+    }
+    return ilosc_Zaglowcow;
 }
